4ebc45b334dcec8d77b64cef950005225515af24
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-02-29 11:38:47 +0200

    Link Database

---
+++ Repository/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options): base(options){}
+
+
+    }
+}
