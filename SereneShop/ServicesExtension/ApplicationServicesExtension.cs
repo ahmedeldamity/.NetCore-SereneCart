fb35f07a70545c8c1ba683cc5e21d1efde2a5bf4
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-02-29 11:09:39 +0200

    Cleaning up the Startup class

---
+++ SereneShop/ServicesExtension/ApplicationServicesExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace API.ServicesExtension
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+
+    }
+}
