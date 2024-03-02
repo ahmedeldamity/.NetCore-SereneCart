72f17fef48ac63e62f65934f0972b74fc6d308dc
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-03-02 12:35:20 +0200

    Redis server - Basket Repository

---
+++ Core/Entities/Basket Entities/BasketItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities.Basket_Entities
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Price must be greater than zero!")]
+        public decimal Price { get; set; }
+
+        public string ImageCover { get; set; }
+
+        public string[] Images { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public decimal RatingsAverage { get; set; }    
+
+        public string Category { get; set; }
+
+        public string Brand { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least one item!")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ Core/Interfaces/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Basket_Entities;
+
+namespace Core.Interfaces.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<Basket?> CreateOrUpdateBasketAsync(Basket basket);
+        Task<Basket?> GetBasketAsync(string basketId);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
