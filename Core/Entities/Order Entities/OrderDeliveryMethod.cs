82512313eb3c18a0def1ab48f37dc9c2231ef271
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-03-05 10:30:47 +0200

    Order Entities

---
+++ Core/Entities/Order Entities/OrderDeliveryMethod.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Entities.Order_Entities
+{
+    public class OrderDeliveryMethod : BaseEntity
+    {
+        public OrderDeliveryMethod()
+        {
+
+        }
+        public OrderDeliveryMethod(string name, string description, decimal cost, string deliveryTime)
+        {
+            Name = name;
+            Description = description;
+            Cost = cost;
+            DeliveryTime = deliveryTime;
+        }
+
+        public string Name { get; set; } // the name of delivery way
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public string DeliveryTime { get; set; } // the time delivery will take to bring the order
+    }
+}
--- Core/Interfaces/Repositories/IWishlistRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Entities;
-
-namespace Core.Interfaces.Repositories
-{
-    public interface IWishlistRepository<T> where T : EntityWithStrId
-    {
-        Task AddAsync(T entity);
-    }
-}
