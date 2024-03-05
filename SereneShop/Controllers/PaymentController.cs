3ff4771cdf4e759713fbb7e685eb0dd273efe3c9
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-03-05 11:05:20 +0200

    Payment controller

---
+++ SereneShop/Controllers/PaymentController.cs
@@ -0,0 +1,33 @@
+﻿using API.Errors;
+using Core.Entities.Basket_Entities;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class PaymentController : BaseApiController
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [ProducesResponseType(typeof(Basket), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [HttpPost("{basketId}")]
+        [Authorize]
+        public async Task<ActionResult<Basket>> CreateOrUpdatePaymentIntend(string basketId)
+        {
+            var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+
+            if (basket is null)
+                return BadRequest(new ApiResponse(400));
+
+            return Ok(basket);
+        }
+    }
+}
